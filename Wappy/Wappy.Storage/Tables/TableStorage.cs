9c1ea3ee46c131fee22b29208b99ca5d4f01bd4d
K. Scott Allen <scott@odetocode.com>
2014-03-14 00:45:22 -0400

    basic table storage abstractions

---
+++ Wappy/Wappy.Storage/Tables/TableStorage.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Wappy.Storage.Tables
+{
+    /// <summary>
+    /// A class for Azure Table Storage access
+    /// </summary>
+    /// <typeparam name="T">T is a TableEntity type to save and query</typeparam>
+    public class TableStorage<T> where T : ITableEntity, new()
+    {
+        /// <summary>
+        /// Connect to a table in table storage, the table is created if it doesn't exist
+        /// </summary>
+        /// <param name="tableName">Name of the table</param>
+        /// <param name="connectionName">Name of the storage connection string in app.config</param>
+        public TableStorage(string tableName, string connectionName = "StorageConnectionString")
+        {
+            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting(connectionName));
+            var tableClient = storageAccount.CreateCloudTableClient();
+
+            Table = tableClient.GetTableReference(tableName);
+            Table.CreateIfNotExists();
+        }
+
+        /// <summary>
+        /// Insert a new entity into table storage
+        /// </summary>
+        /// <param name="entity">The entity to insert</param>
+        /// <returns>The ETag for the new entity</returns>
+        public virtual string Insert(T entity)
+        {
+            var operation = TableOperation.Insert(entity);
+            var result = Table.Execute(operation);
+            return result.Etag;
+        }
+       
+        protected CloudTable Table;
+    }
+}
---
+++ Wappy/Wappy.Storage/Tables/TableStorageQuery.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Wappy.Storage.Tables
+{
+    /// <summary>
+    /// A base class for Table Storage queries. The idea is to derive from this class and 
+    /// modify the Query field with the details of the query to execute. 
+    /// </summary>
+    /// <typeparam name="T">The type of object to return</typeparam>
+    public class StorageQuery<T> where T : ITableEntity, new()
+    {
+        protected TableQuery<T> Query;
+
+        public StorageQuery()
+        {
+            Query = new TableQuery<T>();
+        }
+
+        /// <summary>
+        /// Executes the query and handles continuation tokens
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public virtual IEnumerable<T> ExecuteOn(CloudTable table)
+        {
+            var token = new TableContinuationToken();
+            var segment = table.ExecuteQuerySegmented(Query, token);
+            while (token != null)
+            {
+                foreach (var result in segment)
+                {
+                    yield return result;
+                }
+                segment = table.ExecuteQuerySegmented(Query, token);
+                token = segment.ContinuationToken;
+            }
+        }
+
+        /// <summary>
+        /// A query helper for WHERE value >= from && value <= low
+        /// </summary>
+        /// <param name="key">Name of key to search (PartitionKey, most likely)</param>
+        /// <param name="from">Low value</param>
+        /// <param name="to">High value</param>
+        /// <returns></returns>
+        protected string InclusiveRangeFilter(string key, string from, string to)
+        {
+            var low = TableQuery.GenerateFilterCondition(key, QueryComparisons.GreaterThanOrEqual, from);
+            var high = TableQuery.GenerateFilterCondition(key, QueryComparisons.LessThanOrEqual, to);
+            return TableQuery.CombineFilters(low, TableOperators.And, high);
+        }
+    }
+}
